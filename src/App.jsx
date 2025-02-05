2f084d17e258d5a4018729d5e85a2eb1f48b0746
SeonWoooPark <swpark1234567@naver.com>
2025-02-05 18:04:00 +0900

    feat:Polaroid 등록

--- src/App.jsx
+++ src/App.jsx
@@ -12,28 +12,37 @@ import DeletePolaroid from "./pages/afterLogin/mypage/manage-polaroid/DeletePola
 import ModifyPolaroid from "./pages/afterLogin/mypage/manage-polaroid/ModifyPolaroid";
 import MainLayout from "./style/MainLayout";
 import AlertPage from "./pages/afterLogin/alert/AlertPage.jsx";
+import SignUpPolaroid from "./pages/afterLogin/signUpPolaroid/SignUpPolaroid";
+import FilterPolaroid from "./pages/afterLogin/signUpPolaroid/FilterPolaroid";
+import PrintPolaroid from "./pages/afterLogin/signUpPolaroid/PrintPolaroid";
+import UploadComplete from "./pages/afterLogin/signUpPolaroid/UploadComplete.jsx";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<GlobalLayout />}>
-        {/* 로그인 이전 */}
-        <Route path="/" element={<Home />} />
-        <Route path="check-policy" element={<PrivacyPolicy />} />
-        <Route path="set-nickname" element={<SetNickName />} />
-        {/* 로그인 이후 */}
-        <Route path="main" element={<MainLayout />}>
-          <Route index element={<Main />} />
-          <Route path="alert" element={<AlertPage />} />
-          <Route path="mypage/:id" element={<Mypage />}></Route>
-          <Route path="edit-profile" element={<EditProfile />} />
-          <Route path="manage-polaroid" element={<ManagePolaroid />}>
-            <Route path="modify-polaroid" element={<ModifyPolaroid />} />
-            <Route path="delete-polaroid" element={<DeletePolaroid />} />
+      <Routes>
+        <Route path="/" element={<GlobalLayout />}>
+          {/* 로그인 이전 */}
+          <Route path="/" element={<Home />} />
+          <Route path="check-policy" element={<PrivacyPolicy />} />
+          <Route path="set-nickname" element={<SetNickName />} />
+          {/* 로그인 이후 */}
+          <Route path="main" element={<MainLayout />}>
+            <Route index element={<Main />} />
+            <Route path="alert" element={<AlertPage />} />
+            <Route path="mypage/:id" element={<Mypage />}></Route>
+            <Route path="edit-profile" element={<EditProfile />} />
+            <Route path="manage-polaroid" element={<ManagePolaroid />}>
+              <Route path="modify-polaroid" element={<ModifyPolaroid />} />
+              <Route path="delete-polaroid" element={<DeletePolaroid />} />
+            </Route>
           </Route>
+          {/* 폴라로이드 등록 */}
+          <Route path="signup-polaroid" element={<SignUpPolaroid />} />
+          <Route path="signup-polaroid/filter-polaroid" element={<FilterPolaroid />} />
+          <Route path="signup-polaroid/filter-polaroid/print-polaroid" element={<PrintPolaroid />} />
+          <Route path="signup-polaroid/filter-polaroid/print-polaroid/upload-polaroid" element={<UploadComplete/>} />
         </Route>
-      </Route>
-    </Routes>
+      </Routes>
   );
 }
 
---
+++ src/pages/afterLogin/signUpPolaroid/SignUpPolaroid.jsx
@@ -0,0 +1,118 @@
+import { useState, useEffect, useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import { VscChromeClose } from "react-icons/vsc";
+import { CameraButton } from "../../../components/Button";
+import { CameraIcon, ImageIcon, SelectedCameraIcon, SelectedImageIcon } from "../../../style/Icons";
+
+export default function SignUpPolaroid(props){
+    const navigate = useNavigate();
+    const [source, setSource] = useState("");
+    const [camera, setCamera] = useState(false);
+    const [image, setImage] = useState(false);
+    const [state, setState] = useState(false);
+
+    const fileInputRef = useRef(null);
+
+    const handleCapture = (target) => {
+        if (target.files) {
+            if (target.files.length !== 0) {
+                const file = target.files[0];
+                const newUrl = URL.createObjectURL(file);
+                setSource(newUrl);
+            }
+        }
+    };
+
+    const openCamera = () => {
+        if (fileInputRef.current) {
+        fileInputRef.current.click();
+        }
+    };
+
+    const selectedCamera = () => {
+        setCamera(!camera);
+        setImage(false);
+    }
+
+    const selectedImage = () => {
+        setImage(!image);
+        setCamera(false);
+    }
+
+    useEffect(() => {
+        if (camera || image) {
+            setState(true);
+        } else {
+            setState(false);
+        }
+    },[image, camera])
+
+    useEffect(() => {
+        if (source !== ""){
+            navigate('filter-polaroid', { state : { imageUrl: source}})
+        }
+    }, [source])
+
+    return(
+        <>
+            <div className="flex flex-col h-full pt-4 items-center">
+                <div className="w-full flex flex-row">
+                    <button
+                        className="
+                        w-5
+                        text-2xl
+                        relative
+                        mb-10
+                        "
+                        onClick={() => {
+                            navigate(-1);
+                        }}
+                    >
+                        <VscChromeClose />
+                    </button>
+                    <div />
+                </div>
+                <div className="flex flex-col items-center mb-[56px] my-[124px]">
+                    <p className="text-xl mb-3 font-bold">폴라로이드 등록</p>
+                    <p className="text-[#969696]">사진을 등록할 방식을 선택해주세요!</p>
+                </div>
+                <div className="flex flex-row items-center mb-20">
+                    <div className="mr-4"
+                        onClick={() => selectedCamera()}>
+                        {camera ? <SelectedCameraIcon/> : <CameraIcon/>}
+                    </div>
+                    <div onClick={()=>selectedImage()}>
+                        {image ? <SelectedImageIcon/> : <ImageIcon/>}
+                    </div>
+                </div>
+                <div className="flex-grow"></div>
+                <div className="w-full mb-4">
+                    <CameraButton onCameraOpen={openCamera} title={"다음"} state={state}/>
+                    {camera
+                        ?
+                        <input
+                            type="file"
+                            accept=".png, .jpg"
+                            capture="camera" // 후면 카메라 사용 (없으면 기본 카메라)
+                            style={{ display: "none" }}
+                            ref={fileInputRef}
+                            onChange={(e) => {
+                                handleCapture(e.target);
+                            }}
+                        />
+                        :
+                        <input
+                            type="file"
+                            accept=".jpg, .png"
+                            style={{ display: "none" }}
+                            ref={fileInputRef}
+                            onChange={(e) => {
+                                handleCapture(e.target);
+                            }}
+                        />
+                    }       
+                </div>
+            </div>
+        </>
+    );
+}
