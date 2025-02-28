bda6780921d273f0368093d021f538910185b050
strdeok <ejraks1548@gmail.com>
2025-02-28 16:55:15 +0900

    feat: 1대1 문의 및 리뷰 작성 시, 카카오 오픈채팅 연결

--- src/pages/afterMain/mypage/components/ThirdSection.jsx
+++ src/pages/afterMain/mypage/components/ThirdSection.jsx
@@ -1,8 +1,11 @@
+import { Link } from "react-router-dom";
+
 export default function ThirdSection() {
   return (
     <div className="my-6 flex flex-col gap-6">
-      <p>1:1 문의하기</p>
-      <p>리뷰 남기기</p>
+      <Link to={"https://open.kakao.com/o/sSkSqFih"}>
+        1:1 문의 및 리뷰 남기기
+      </Link>
     </div>
   );
 }
