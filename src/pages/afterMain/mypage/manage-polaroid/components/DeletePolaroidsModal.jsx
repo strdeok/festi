5193e861a2ac3652341a7aae97f7798b3c18a730
strdeok <ejraks1548@gmail.com>
2025-03-07 20:25:08 +0900

    feat: 폴라로이드 삭제 추가

--- src/pages/afterMain/mypage/manage-polaroid/components/DeletePolaroidsModal.jsx
+++ src/pages/afterMain/mypage/manage-polaroid/components/DeletePolaroidsModal.jsx
@@ -1,14 +1,15 @@
 import { useNavigate } from "react-router-dom";
+import { DeletePolarod } from "../../../../../util/DeletePolaroid";
 
-export default function DeletePolaroidsModal({ image }) {
+export default function DeletePolaroidsModal({ image, setFullimage }) {
   const navigate = useNavigate();
   return (
     <div className="fixed top-0 left-0 bg-black bg-opacity-25 w-full h-full z-10 flex items-center justify-center">
       <div className="fixed z-20 w-[19rem] h-[25.75rem] flex flex-col gap-4 items-center justify-center bg-white rounded-2xl">
         <p className="mt-8 font-bold text-lg">폴라로이드 삭제</p>
         <img
-          src={image}
-          alt={image}
+          src={image.imgLink}
+          alt={image.imgLink}
           className="w-[8.6878rem] h-[10.4375rem] object-fill"
         />
         <p className="px-14 text-center text-[#969696] text-sm">
@@ -24,7 +25,10 @@ export default function DeletePolaroidsModal({ image }) {
           >
             취소
           </button>
-          <button className="py-3 px-12 border border-[#FC5852] text-white bg-[#FC5852] rounded-lg">
+          <button className="py-3 px-12 border border-[#FC5852] text-white bg-[#FC5852] rounded-lg"
+            onClick={()=>{
+              DeletePolarod(image ,setFullimage)
+            }}>
             삭제
           </button>
         </div>
