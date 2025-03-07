b4f04925b50d33334ae51f190ccf9d1e4c30d1f2
strdeok <ejraks1548@gmail.com>
2025-03-07 19:47:03 +0900

    fix: 닉네임 수정 시, 중복체크 느리게 되는 것 수정. 비동기 함수로 수정

--- src/util/CheckNickNameExist.jsx
+++ src/util/CheckNickNameExist.jsx
@@ -1,13 +1,14 @@
 import axios from "axios";
 
-export const CheckNickNameExist = (nickname, setExist) => {
-  axios
+export const CheckNickNameExist = async (nickname, setExist) => {
+  await axios
     .get(`/v1/api/user/nickname/check?nickname=${nickname}`, {
       headers: {
         Authorization: `Bearer ${localStorage.getItem("jwtToken")}`,
       },
     })
     .then((res) => {
+      console.log(res);
       if (res.data.success) {
         setExist(false);
       } else setExist(true);
